df81063448998dcf057c8795e39929d3bf5075fc
nmn29 <ina010218@gmail.com>
2021-11-21 17:23:33 +0900

    fix

--- cogs/symmetry.py
+++ cogs/symmetry.py
@@ -103,12 +103,12 @@ class Symm(commands.Cog):
             imgr_1 = imgr.crop((imgr.size[0] // haba, 0, imgr.size[0], imgr.size[1]))
             imgr_2 = ImageOps.mirror(imgr_1)
 
-            syml = Image.new('RGBA', (imgr_1.width + imgr_2.width, imgr_1.height))
-            syml.paste(imgr_2, (0, 0))
-            syml.paste(imgr_1, (imgr_1.width, 0))
+            symr = Image.new('RGBA', (imgr_1.width + imgr_2.width, imgr_1.height))
+            symr.paste(imgr_2, (0, 0))
+            symr.paste(imgr_1, (imgr_1.width, 0))
 
             b = io.BytesIO()
-            syml.save(b, format='PNG')
+            symr.save(b, format='PNG')
             bs.append(io.BytesIO(b.getvalue()))
 
         return bs
